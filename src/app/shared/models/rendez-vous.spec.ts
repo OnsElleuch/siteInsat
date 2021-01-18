551ee1c119bfd4cd334cd86f3d0a2759d2825b73
unknown <ons.elleuch.oe@gmail.com>
2021-01-18 19:26:58 +0100

    basic about us page , static

---
+++ src/app/shared/models/rendez-vous.spec.ts
@@ -0,0 +1,7 @@
+import { RendezVous } from './rendez-vous';
+
+describe('RendezVous', () => {
+  it('should create an instance', () => {
+    expect(new RendezVous()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/ressource.ts
@@ -0,0 +1,4 @@
+export class Ressource {
+    public name : string;
+    public url : string;
+}
