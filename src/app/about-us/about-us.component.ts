d25b98c8ebac9041be64243d5bb7ac6d48a228ba
Ons Elleuch <ons.elleuch.oe@gmail.com>
2021-01-21 22:31:35 +0100

    fixing

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -89,21 +89,7 @@ export class AboutUsComponent implements OnInit {
     }, timeout);
 
   }
-  initCarousel9(className: string, timeout) {
-    const carousel = jQuery('#' + className);
-
-    setTimeout(function() {
-      carousel.owlCarousel(
-        {
-          items: 3, // 10 items above 1000px browser width
-          itemsDesktop: [1000, 2], // 5 items between 1000px and 901px
-          itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-          itemsTablet: [600, 1], // 2 items between 600 and 0
-          itemsMobile: [400, 1] // itemsMobile disabled - inherit from itemsTablet option
-        }
-      );
-    }, timeout);
-  }
+  
   getDirectionMembers(){
     this.busy = this.aboutService.getDirectionMembers().subscribe(
       (data) => {
