4ae2829d662dd526613b08e09235fe50ac621c5c
Ons Elleuch <ons.elleuch.oe@gmail.com>
2021-01-29 22:51:10 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     DepartementsModule,
     SharedModule,
-    ScolariteModule
+    ScolariteModule,
     FormationModule,
     BrowserAnimationsModule,
 
